9de6221bd38112028d3d7691a66586fd17c2cb50
Alisher Yertayev <alisheryertayev@Alishers-MacBook-Pro.local>
2025-06-20 17:04:21 +0900

    Initial commit

---
+++ publisher.py
@@ -0,0 +1,43 @@
+from config import (
+    get_connection,
+    configure_logging,
+    MQ_EXHANGE,
+    MQ_ROUTING_KEY,
+)
+import logging
+from typing import TYPE_CHECKING
+import time
+
+if TYPE_CHECKING:
+    from pika.adapters.blocking_connection import BlockingChannel
+
+log = logging.getLogger(__name__)
+
+def produce_message(channel: "BlockingChannel") -> None:
+    queue = channel.queue_declare(queue=MQ_ROUTING_KEY)
+    log.info("Declared queue %r %s", MQ_ROUTING_KEY, queue)
+    message_body = f"Hello World from {time.time()}"
+    log.info("Publish message %s", message_body)
+    channel.basic_publish(
+        exchange=MQ_EXHANGE,
+        routing_key=MQ_ROUTING_KEY,
+        body=message_body, 
+    )
+
+def main():
+    configure_logging()
+    with get_connection() as connection:
+        log.info("Created connection: %s", connection)
+        with connection.channel() as channel:
+            log.info("Created channel: %s", channel)
+            produce_message(channel=channel)
+            
+
+    while True:
+        pass
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        log.warning("Ты кто такой? Давай, до свидания!")
